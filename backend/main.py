976ccb6e2d02e4defca714cba6c6979272dc1e7b
Prashanth S N <prashanthsn@Prashanths-MacBook-Air.local>
2026-06-20 14:05:06 +0530

    Add Vercel CORS

--- backend/main.py
+++ backend/main.py
@@ -36,7 +36,8 @@ ALGORITHM = os.getenv("ALGORITHM")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173",
-                    "http://localhost:5174","*"],
+                    "http://localhost:5174",
+                    "https://luminachat-vert.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
